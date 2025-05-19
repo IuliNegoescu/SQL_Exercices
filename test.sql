14391456315c0b199053168dcb4cd11a94032978
Iuli Negoescu <macbook@macbooks-MacBook-Pro.local>
2025-05-19 14:10:57 +0300

    Second commit

--- test.sql
+++ test.sql
@@ -11,12 +11,33 @@ INSERT INTO Users(ID, Name, Age, Country)
 VALUES (1, 'Alice', 18, 'Romania'),
     (2, 'Iulian', 21, 'Spain'),
     (3, 'John', 45, 'Germany');
---Test Update statement
+    /*
+
+
+    --Test Update statement
 UPDATE Users
 SET Name = 'Iuli',
     Age = 11
 WHERE ID > 1;
-SELECT *
+
+
+    --Test Delete Statement
+DELETE FROM Users
+WHERE ID = 1;
+
+
+    --Test SQL Aggregate Functions--
+ SELECT MIN(Age),Name
 FROM Users;
+
+
+    --Test MAX SQL statement--
+
+  SELECT MAX(Age),Name
+FROM Users;
+*/
+
+SELECT * FROM Users;
+
 --Ca sa vad datele din tabel trebuie sa selectez numai linia 10
 --Daca selectez toate liniile de cod nu voi vedea datele din tabel
